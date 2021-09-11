776d0ec9fa089daa6eefbd6b7186301259f578aa
jdr4343 <jdr4343@naver.com>
2021-09-12 03:37:09 +0900

    HomeView make it

---
+++ ios100ToyExample/Login&registe&database/Controller/CoreTabs/Home/HomeHeaderViewController.swift
@@ -0,0 +1,26 @@
+//
+//  HomeHeaderViewController.swift
+//  ios100ToyExample
+//
+//  Created by 신지훈 on 2021/09/12.
+//
+
+import UIKit
+
+class HomeHeaderViewController: UIView {
+
+    static let identifier = "HomeHeaderViewController"
+    
+    
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+
+}
--- ios100ToyExample/Login&registe&database/Controller/CoreTabs/Home/HomeViewController.swift
+++ ios100ToyExample/Login&registe&database/Controller/CoreTabs/Home/HomeViewController.swift
@@ -10,11 +10,21 @@ import FirebaseAuth
 
 class HomeViewController: UIViewController {
 
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        
-        
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        navigationItem.largeTitleDisplayMode = .always
     }
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
@@ -24,7 +34,7 @@ class HomeViewController: UIViewController {
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        view.frame = view.bounds
+        tableView.frame  = view.bounds
     }
     
     // 사용자가 로그인 하지 않았다면 로그인 화면을 보여주고 아니라면 메인 뷰를 보여줄것입니다.
@@ -35,4 +45,26 @@ class HomeViewController: UIViewController {
             present(loginVC, animated: true)
         }
     }
+}
+
+extension HomeViewController: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Test"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
 }
