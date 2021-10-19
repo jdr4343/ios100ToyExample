f6fbca43d8b2e60148843e56102e642b8a54f7eb
jdr4343 <jdr4343@naver.com>
2021-10-19 16:36:47 +0900

    update

--- ios100ToyExample/Example/EventKit/EventKitViewController.swift
+++ ios100ToyExample/Example/EventKit/EventKitViewController.swift
@@ -6,15 +6,54 @@
 //
 
 import UIKit
+import EventKit
+import EventKitUI
 
-class EventKitViewController: UIViewController {
-
+class EventKitViewController: UIViewController, EKEventViewDelegate {
+  
+    let store = EKEventStore()
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        title = "이벤트 킷"
+        view.backgroundColor = .systemBackground
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add,
+                                                            target: self,
+                                                            action: #selector(didTapAdd))
     }
-    
+   
+    @objc private func didTapAdd() {
+        ///사용자에게 권한을 요청하고 권한이 허용된다면 이벤트 페이지를 오픈 합니다.
+        store.requestAccess(to: .reminder) { [weak self] success, error in
+            if success,error == nil {
+                DispatchQueue.main.async {
+                    guard let store = self?.store else { return }
+                    let newEvent = EKEvent(eventStore: store)
+                    newEvent.title = "유튜브 비디오"
+                    newEvent.startDate = Date()
+                    newEvent.endDate = Date()
+                    
+                    let otherVC = EKEventEditViewController()
+                    otherVC.eventStore = store
+                    otherVC.event = newEvent
+                    
+                
+                    
+                    self?.present(otherVC, animated: true, completion: nil)
+                    
+//
+//                    let vc = EKEventViewController()
+//                    vc.delegate = self
+//                    vc.event = newEvent
+//                    let navVC = UINavigationController(rootViewController: vc)
+//                    self?.present(navVC, animated: true, completion: nil)
+                }
+            }
+        }
+        
+      
+    }
 
-
+    func eventViewController(_ controller: EKEventViewController, didCompleteWith action: EKEventViewAction) {
+        
+    }
 }
